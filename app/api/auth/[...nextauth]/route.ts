345533833a1b8133ef273665d9525d6801962ccd
Nimra Nisa <168928017+nimranisa4@users.noreply.github.com>
2024-12-02 13:52:50 +0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,5 @@
 import { handlers } from "@/app/lib/auth";
 
 export const {POST,GET} = handlers;
-export async function auth() {
-    // Mocking session data
-    return { user: { name: "Nimra Nisa" } }; // Replace `null` with user data if logged out
-  }
+
   
